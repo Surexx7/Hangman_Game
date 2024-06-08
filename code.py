9104d75b2aec4e72c2410f253479f0676149490d
Suresh Shahi <138643368+Surexx7@users.noreply.github.com>
2024-06-08 20:13:47 +0545

    complete

---
+++ code.py
@@ -0,0 +1,38 @@
+import random
+import hangman_stages
+import word_lists
+print("\t\t\t\t\t\t\t\t\t***HANGMAN GAME***")
+print("You have only 6 lives so try to guess word within 6 attempts! Good Luck")
+lives=6
+chosen_words=str(random.choice(word_lists.word))
+chosen_word=chosen_words.lower()
+display=[]
+
+#to disply the blank space as the letter that a chosen word have
+#for letter in chosen_word:
+for i in range(len(chosen_word)):
+    display +="_"
+print(display)
+game_over=False
+while not game_over:
+  guessed_letter=input("Guess a Letter: ").lower()
+  for position in range(len(chosen_word)):
+      letter=chosen_word[position]
+      if letter==guessed_letter:
+          display[position]=guessed_letter
+  print(display)
+  if guessed_letter not in chosen_word:
+      lives -=1
+      if lives==0:
+          game_over=True
+          print("Sorry!! You Lose 😔😔😔")
+          print("The word was: ",chosen_word)
+  if "_" not in display:
+      game_over=True
+
+  print(hangman_stages.stages[lives])
+else:
+    print("Wow Congratulation!! You Won..👏👏👏")
+
+
+
---
+++ word_lists.py
@@ -0,0 +1,8 @@
+word=["Adventure", "Elephant", "Galaxies", "Harmony", "Notebook", "Pancakes",
+           "Rainbow", "Sunshine", "Treasure", "Volcano", "Butterfly", "Chocolate",
+           "Dinosaur", "Enchanted", "Fireworks", "Giraffe", "Holiday", "Jasmine", "Kangaroo",
+           "Lighthouse", "Mystery", "Nightmare", "Oxygen", "Pyramid", "Quicksand", "Rocketship",
+           "Starfish", "Tornado", "Umbrella", "Violet", "Waterfall", "Xylophone", "Lion", "Tiger", "Zebra",
+           "Koala", "Penguin", "Leopard", "Gorilla", "Cheetah", "Flamingo", "Rhinoceros",
+           "Apple", "Banana", "Cherry", "Dragonfruit", "Fig", "Grapes",
+           "India", "Japan", "Kenya", "Luxembourg", "Mexico", "Norway", "Oman", "Portugal","patato","beautiful"]
